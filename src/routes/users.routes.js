2869ef3f7b92c25bd5313579382fedf7b4c7f04c
David Bass <andres.a97@hotmail.com>
2026-04-14 20:20:19 -0500

    se exponen todos los endpoints relacionados a usuario

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
 const userService = require('../services/user.service');
 
+//listar conductores
 router.get('/listDrivers', authMiddleware, async (req, res) => {
     try {
         const users = await userService.listDrivers({
@@ -17,10 +18,24 @@ router.get('/listDrivers', authMiddleware, async (req, res) => {
         res.status(500).json({ message: 'Error al listar usuarios' });
     }
 });
-
+//listar supervisores
 router.get('/listSupervisor', authMiddleware, async (req, res) => {
     try {
         const users = await userService.listSupervisor({
+            userId: req.user.id,
+            rol: req.user.rol,
+        });
+
+        res.json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al listar usuarios' });
+    }
+});
+//listar todos los usuarios
+router.get('/listUsers', authMiddleware, async (req, res) => {
+    try {
+        const users = await userService.listUsers({
             userId: req.user.id,
             rol: req.user.rol,
         });
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -79,3 +79,41 @@ ORDER BY u.id_usuario;
     throw error;
   }
 };
+
+//listar todos los usuarios
+exports.listUsers = async ({ rol }) => {
+  try {
+
+    if (rol !== "admin") {
+      throw new Error("No autorizado");
+    }
+
+    const query = `
+     SELECT 
+  u.id_usuario,
+  u.nombre_completo,
+  r.nombre AS rol,
+  u.id_vehiculo,
+  v.placa,
+  uc.nombre AS nombre
+FROM usuario u
+LEFT JOIN rol r 
+  ON u.rol_id = r.id_rol
+LEFT JOIN vehiculo v 
+  ON u.id_vehiculo = v.id_vehiculo
+LEFT JOIN cliente uc 
+  ON u.cliente_id = uc.id_cliente
+ORDER BY u.id_usuario;
+    `;
+
+    const result = await pool.query(query);
+
+    return {
+      users: result.rows || []
+    };
+
+  } catch (error) {
+    console.error("Error en listar usuarios:", error.message);
+    throw error;
+  }
+};
