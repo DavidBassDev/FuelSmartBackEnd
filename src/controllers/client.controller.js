189be68afe28fa5b865a1526b94457244c516b63
David Bass <andres.a97@hotmail.com>
2026-04-28 22:12:00 -0500

    se agrega regla de negocio para traer placas por cliente

--- src/controllers/client.controller.js
+++ src/controllers/client.controller.js
@@ -12,19 +12,22 @@ exports.getClient = async (req, res) => {
 // TRAER PLACAS POR CLIENTE
 exports.getPlatesByClient = async (req, res) => {
   try {
-    const { id_cliente } = req.body;
-    if (!id_cliente) {
+    const { id_cliente, rol } = req.query;
+
+    // Solo validar id_cliente si NO es admin
+    if (parseInt(rol) !== 1 && !id_cliente) {
       return res.status(400).json({
         ok: false,
         error: "id_cliente es requerido"
       });
     }
 
-    const data = await service.getPlatesByClient({ id_cliente });
-
-    res.json({
-      data
+    const data = await service.getPlatesByClient({
+      id_cliente: id_cliente ? parseInt(id_cliente) : null,
+      rol: parseInt(rol),
     });
+
+    res.json({ data });
 
   } catch (error) {
     res.status(500).json({ error: error.message });
--- src/services/client.service.js
+++ src/services/client.service.js
@@ -21,17 +21,35 @@ exports.getClient = async () => {
 
 
 //TRAER CLIENTES CON CANTIDAD DE PLACAS
-exports.getPlatesByClient = async ({ id_cliente }) => {
-  const result = await pool.query(
-    `SELECT c.nombre AS cliente,
-            COUNT(v.id_vehiculo) AS cantidad_placas
-     FROM vehiculo v
-     JOIN usuario u ON v.usuario_id = u.id_usuario
-     JOIN cliente c ON u.cliente_id = c.id_cliente
-     WHERE c.id_cliente = $1
-     GROUP BY c.nombre;`,
-    [id_cliente]
-  );
-console.log("ID en service:", id_cliente);
+exports.getPlatesByClient = async ({ id_cliente, rol }) => {
+  let result;
+
+  if (rol === 1) {
+    result = await pool.query(`
+      SELECT c.id_cliente,
+             c.nombre AS cliente,
+             COUNT(v.id_vehiculo) AS cantidad_placas
+      FROM cliente c
+      LEFT JOIN usuario u ON u.cliente_id = c.id_cliente
+      LEFT JOIN vehiculo v ON v.usuario_id = u.id_usuario
+      GROUP BY c.id_cliente, c.nombre
+      ORDER BY c.nombre ASC;
+    `);
+
+  } else {
+    
+    result = await pool.query(
+      `SELECT c.id_cliente,
+              c.nombre AS cliente,
+              COUNT(v.id_vehiculo) AS cantidad_placas
+       FROM vehiculo v
+       JOIN usuario u ON v.usuario_id = u.id_usuario
+       JOIN cliente c ON u.cliente_id = c.id_cliente
+       WHERE c.id_cliente = $1
+       GROUP BY c.id_cliente, c.nombre;`,
+      [id_cliente]
+    );
+  }
+
   return result.rows || [];
 };
